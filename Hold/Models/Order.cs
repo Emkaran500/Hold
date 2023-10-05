23c5c17dff3ea17ecd333b38671d0376b7fefccc
Emil B <emkaran_500@mail.ru>
2023-10-05 16:24:06 +0400

    Add Views

---
+++ Hold/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Hold.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Courier? Courier { get; set; }
+    public ObservableCollection<Product>? Product { get; set; }
+}
---
+++ Hold/Models/OrderHistory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hold.Models;
+
+public class OrderHistory
+{
+    public int Id { get; set; }
+    public Order? Order { get; set; }
+    public DateTime TimeOfOrder { get; set; }
+}
